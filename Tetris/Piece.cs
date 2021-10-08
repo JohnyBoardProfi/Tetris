eb85206e68778fe7e2fcea32d48613d024f4d14e
JohnyBoardProfi <91531366+JohnyBoardProfi@users.noreply.github.com>
2021-10-08 18:47:26 +0300

    Add files via upload

---
+++ Tetris/Piece.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+namespace Tetris
+{
+    public class Piece
+    {
+        private int[,] _piece;
+        private int _initPosX, _initPosY;
+
+        public Piece(int[,] p)
+        {
+            if (p == null)
+            {
+                throw new ArgumentNullException();
+            }
+            _piece = (int[,])p.Clone();
+            _initPosY = (p.GetUpperBound(0) + 1) * -1;
+            _initPosX = 0;
+        }
+
+        public int Height => _piece.GetUpperBound(0) + 1;
+
+        public int Width => _piece.GetUpperBound(1) + 1;
+
+        public int InitPosX => _initPosX;
+
+        public int InitPosY => _initPosY;
+
+        public Piece RotateRight()
+        {
+            int[,] rotated = new int[this.Width, this.Height];
+            for (int i = 0; i < Height; i++)
+            {
+                for (int j = 0; j < Width; j++)
+                {
+                    rotated[j, Height - i - 1] = _piece[i, j];
+                }
+            }
+            return new Piece(rotated);
+        }
+
+        public void MakeItShadow()
+        {
+            for (int i = 0; i < this.Height; i++)
+            {
+                for (int j = 0; j < this.Width; j++)
+                {
+                    if (this[i, j] != 0)
+                    {
+                        _piece[i, j] = 8;
+                    }
+                }
+            }
+        }
+
+        public int[,] ToArray() => _piece;
+
+        public int this[int h, int w] => (h < 0) || (h >= this.Height) || (w < 0) || (w >= this.Width) ? throw new IndexOutOfRangeException("Index is out of range!") : _piece[h, w];
+
+        public object Clone() => new Piece(this._piece);
+    }
+}
---
+++ Tetris/PieceFactory.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Tetris
+{
+    public class PieceFactory
+    {
+        private static List<Piece> _pieces;
+
+        static PieceFactory() => Initialize();
+
+        public static Piece GetPiecebyId(int id) => _pieces.Count > id && id >= 0 ? _pieces[id] : null;
+
+        public static Piece GetRandomPiece(Random r) => GetPiecebyId(r.Next(_pieces.Count));
+
+        public static int Count => _pieces.Count;
+
+        public static void Initialize()
+        {
+            _pieces = new List<Piece>()
+            {
+                new Piece(new int[,] { { 1, 1, 1, 1 } }),
+                new Piece(new int[,] { { 2, 2 }, { 2, 2 } }),
+                new Piece(new int[,] { { 0, 0, 3 }, { 3, 3, 3 } }),
+                new Piece(new int[,] { { 4, 0, 0 }, { 4, 4, 4 } }),
+                new Piece(new int[,] { { 0, 5, 5 }, { 5, 5, 0 } }),
+                new Piece(new int[,] { { 6, 6, 0 }, { 0, 6, 6 } }),
+                new Piece(new int[,] { { 6, 6, 0 }, { 0, 6, 6 } }),
+                new Piece(new int[,] { { 7, 7, 7 }, { 0, 7, 0 } })
+            };
+        }
+    }
+}
